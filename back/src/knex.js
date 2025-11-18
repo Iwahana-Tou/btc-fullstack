434e86985bb4c231fc338f959aff9088ad7a7868
to-kun <to_iwahana@mail.toyota.co.jp>
2025-11-18 20:43:51 +0900

    development setting

--- back/src/knex.js
+++ back/src/knex.js
@@ -1,4 +1,17 @@
-const config = require("../knexfile");
-const knex = require("knex")(config);
+// const config = require("../knexfile");
+// const knex = require("knex")(config);
 
-module.exports = knex;
+// module.exports = knex;
+
+const knex = require('knex');
+const knexConfig = require('../knexfile');
+
+let env = knexConfig.development;
+
+if (process.env.NODE_ENV === 'production') {
+  env = knexConfig.production;
+}
+
+console.log(process.env.NODE_ENV);
+
+module.exports = knex(env);
